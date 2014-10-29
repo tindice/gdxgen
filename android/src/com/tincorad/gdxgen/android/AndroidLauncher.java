13be91a9e8ebe20cf8ccd995a491f27de7750606
Martin Leibner <tincorad@gmail.com>
2014-10-29 20:54:02 -0200

    Preparando proyecto

---
+++ android/src/com/tincorad/gdxgen/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tincorad.gdxgen.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tincorad.gdxgen.GdxGenGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxGenGame(), config);
+	}
+}
